9890d4ef0327aac739374ef0b969547041182a7e
Sasha <popowuch1104@gmail.com>
2021-10-24 18:12:21 +0200

    install classnames library

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './containers/App/App.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <h1 className="header">Hello Index</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
